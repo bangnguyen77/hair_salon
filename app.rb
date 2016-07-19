521d29b19fe18336f1d066593493b04bcb4483b5
Bang Nguyen <bangnv77@gmail.com>
2016-07-18 20:36:05 -0700

    delet functionality works

--- app.rb
+++ app.rb
@@ -38,3 +38,10 @@ patch("/stylists/:id") do
   @stylists = Stylist.all()
   erb(:stylist)
 end
+
+delete("/stylists/:id") do
+  @stylist = Stylist.find(params.fetch("id").to_i())
+  @stylist.delete()
+  @stylists = Stylist.all()
+  erb(:stylists)
+end
