d36e81fdf70c7afbb8fc4edbfa03708139979be3
Bang Nguyen <bangnv77@gmail.com>
2016-07-20 17:51:45 -0700

    final fix for the integration spec

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -4,10 +4,6 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe("the hair salon path", {:type => :feature}) do
-  it("allows users to view the homepage") do
-    visit("/")
-    erb(:index)
-  end
 
   it("allows users to navigate to the stylist page and see the list of stylists") do
     stylist = Stylist.new({:id => nil, :name => "Courtney"})
@@ -16,13 +12,11 @@ describe("the hair salon path", {:type => :feature}) do
     click_link("List of Stylists")
     expect(page).to have_content(stylist.name())
   end
-end
 
-describe("add a new stylist", {:type => feature}) do
-    it("allows users to add a new stylist") do
-      visit("/stylists")
-      fill_in("name", :with => "Jill")
-      click_button("Add a Stylist")
-      expect(page).to have_content("Jill")
-    end
+  it("allows users to add a new stylist") do
+    visit("/stylists")
+    fill_in("name", :with => "Jill")
+    click_button("Add a Stylist")
+    expect(page).to have_content("Jill")
+  end
 end
